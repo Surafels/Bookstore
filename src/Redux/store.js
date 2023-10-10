9751d0c81904152395d31587762597f6f39e77c7
Surafels <ssurafel4@gmail.com>
2023-10-10 19:11:42 +0300

    Add categorySlice  and check status

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
+import categoryReducer from './categories/categoriesSlice';
 
 const store = configureStore({
 
   reducer: {
     books: booksReducer,
-    // catagories: catagoryReducer,
+    catagories: categoryReducer,
   },
 });
 
