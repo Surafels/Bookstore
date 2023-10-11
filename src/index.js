5e90160dfac57c5c71bf6c431c3baf5507461b73
Surafels <ssurafel4@gmail.com>
2023-10-11 17:19:03 +0300

    Wrap App with provider

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import App from './App';
 import store from './Redux/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+      <App />
 
       </Provider>
     </React.StrictMode>
