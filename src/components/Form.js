73e84dc9e27ef4dc285eaf2b1740a2e9e3d7f574
Surafels <ssurafel4@gmail.com>
2023-10-15 01:28:08 +0300

    Add style on book information

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@ import AddBtn from './AddBtn';
 
 const InputItem = () => (
   <div>
+    <div className="horizontal-divider" />
     <h1 className="form-title">ADD NEW BOOK</h1>
     <AddBtn />
   </div>
--- src/components/NavList.js
+++ src/components/NavList.js
@@ -1,16 +1,22 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const NavList = () => (
   <div className="nav-bar">
     <nav>
-      <p>Bookstore CMS</p>
+      <p className="nav-head">Bookstore CMS</p>
       <ul className="nav-list">
         <li className="book-link">
-
           <Link to="/">Books</Link>
         </li>
         <li className="category-link">
-          <Link to="/Categories">Categories </Link>
+          <Link to="/Categories">Categories</Link>
+        </li>
+        <li className="person-link">
+          <Link to="/Profile">
+            <FontAwesomeIcon icon={faUser} />
+          </Link>
         </li>
       </ul>
     </nav>
